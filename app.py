c29971f3ac8a678953dd89aa3dd16d190f788261
vinaykrishna <vinay2210978@gmail.com>
2024-04-04 16:46:35 +0200

    ad

--- app.py
+++ app.py
@@ -202,7 +202,7 @@ role = 'Assistant'
 
 
 def hardcode(message):
-    if "btp" in message:
+    if "it0005518128" in message:
         return """Riusciresti a dirmi qual è il tuo prezzo medio di carico? In caso negativo. il titolo nella giornata di ieri, 28 marzo 2024, ha chiuso ad un prezzo di 106,05€, questo significa che se l’avessi comprato al prezzo minimo del 5 ottobre 2023, pari a 96,85€ il capital gain alla chiusura di ieri sarebbe del 9,912%, mentre se fosse al prezzo massimo pari a 97,45€ massimo il capital gain sarebbe del 9,236%. 
                 Inoltre, per calcolare il tuo guadagno è necessario tenere in considerazione anche il rateo maturato dalla data di acquisto, pari a 180 giorni al tasso di interesse dell’obbligazione pari a 4,40%.
                 Per calcolare con esattezza il tuo guadagno riusciresti a dirmi il prezzo medio di carico che quantità hai acquisito? """
@@ -210,6 +210,8 @@ def hardcode(message):
         return "Ad un prezzo medio di carico di 97€, il capital gain teorico è pari a 9,742%, mentre il rateo maturato è pari a 297,21€."
     elif "risparmi" in message:
         return "Analizzando il tuo profilo di rischio e i tuoi obiettivi, ed ipotizzando un risparmio per l’anno futuro in linea con quello passato e pari a 5mila euro, allora ti suggerirei di investire in un piano di accumulo con un orizzonte temporale di 5-7 anni, in modo da sfruttare il cost average, investendo inizialmente una cifra di 2000€ e versando mensilmente circa 200 euro. La restante parte del risparmio in obbligazioni a basso rischio e legate all’inflazione con un orizzonte più breve pari a 1-3 anni."
+    elif "spese" in message:
+        return plotter()
     else:
         return None
 
@@ -321,6 +323,39 @@ def generate_plot(category_diff):
     plt.close()
 
     return plot_data
+
+
+
+
+def plotter():
+    files=os.listdir(app.config['UPLOAD_FOLDER'])
+    # Filter out only .xlsx files
+    xlsx_files = [file for file in files if file.endswith('.xlsx')]
+    file_path= xlsx_files[0]
+    df = pd.read_excel(file_path)  # Assuming the file is an Excel file
+    # Convert 'Data' column to datetime format
+    df['Data'] = pd.to_datetime(df['Data'], format='%d/%m/%Y')
+
+    # Extract month and year from the 'Data' column
+    df['Month'] = df['Data'].dt.month
+    df['Year'] = df['Data'].dt.year
+
+    # Define categories of expenses
+    categories = ['Utenze', 'Alimentari', 'Trasporti', 'Hotel Ristoranti e Viaggi', 'Giochi Cultura e Spettacoli', 'Mutui, finanziamenti e assicurazioni', 'Altre spese']
+
+    # Create a pivot table for each category spending in each month
+    pivot_table = df.pivot_table(index='Moneymap', columns=['Year', 'Month'], values='Movimento', aggfunc='sum', fill_value=0)
+
+    # Filter the pivot table for categories of interest
+    category_pivot = pivot_table.loc[categories]
+
+    # Calculate the difference from the previous month
+    category_diff = category_pivot.diff(axis=1)
+    res=generate_plot(category_diff)
+    rjson= {"rtype":"image","result":res}
+    return jsonify(str(rjson))
+
+
 
 
 
